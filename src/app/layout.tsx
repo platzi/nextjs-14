65c95162e26a2caab7791d9bd88cbc50662a9c6a
edevars <e.devars.dev@gmail.com>
2023-11-07 11:21:18 -0600

    feat: layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,8 +21,12 @@ export default function RootLayout({
         <header>
           <nav>
             <ul>
-              <li>Home</li>
-              <li>Store</li>
+              <Link href="/">
+                <li>Home</li>
+              </Link>
+              <Link href="/store">
+                <li>Store</li>
+              </Link>
             </ul>
           </nav>
         </header>
