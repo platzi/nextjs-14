d368fa803ee79ddcf69127f94eacd0c38f590812
edevars <e.devars.dev@gmail.com>
2023-11-08 09:17:46 -0600

    feat: adding sass globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import { Header } from 'app/components/shared/Header'
 import { Footer } from 'app/components/shared/Footer'
+import 'app/sass/globals.sass'
 
 const inter = Inter({ subsets: ['latin'] })
 
