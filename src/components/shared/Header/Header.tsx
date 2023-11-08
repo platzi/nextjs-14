804f52025c66ae5b172c7d66b5e906d41d502f22
edevars <e.devars.dev@gmail.com>
2023-11-08 08:46:50 -0600

    feat: preapending styles

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,17 +1,22 @@
 import Link from 'next/link'
+import styles from './Header.module.css'
 
 export const Header = () => {
-  console.log('Hola mundo header')
-  return (<header>
-    <nav>
-      <ul>
-        <Link href="/">
-          <li>Home</li>
-        </Link>
-        <Link href="/store">
-          <li>Store</li>
-        </Link>
-      </ul>
-    </nav>
-  </header>)
+  return (
+    <header>
+      <nav>
+        <ul className={styles.Header__list}>
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/store">
+              Store
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>)
 }
