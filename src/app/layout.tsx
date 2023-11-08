30c0578f9232c01f8452f3b646e9e22e138a6846
edevars <e.devars.dev@gmail.com>
2023-11-08 11:10:44 -0600

    feat: nex fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import { Header } from 'app/components/shared/Header'
 import { Footer } from 'app/components/shared/Footer'
 import 'app/sass/globals.sass'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ["100", "300","500", "700"],
+  subsets: ["latin"],
+})
 
 export default function RootLayout({
   children,
@@ -12,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
