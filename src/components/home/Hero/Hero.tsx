da8f91f733721a77e1e3b6550eaa2c53f03a2b09
edevars <e.devars.dev@gmail.com>
2023-11-08 08:38:46 -0600

    feat: css modules

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,11 @@
+import styles from './Hero.module.css'
+
 export const Hero = () => {
-  return(
-    <section>
-      <h1>Hero</h1>
+  
+  return (
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
     </section>
   )
 }
