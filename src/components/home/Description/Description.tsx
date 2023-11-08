afe8a7552f026da6ff5149ec78deabddcfff93b0
edevars <e.devars.dev@gmail.com>
2023-11-08 10:23:57 -0600

    feat: next image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,15 @@
 import styles from './Description.module.sass';
+import Image from 'next/image';
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <img src="/images/description.jpeg" alt="products marketplace" />
+      <Image
+        src="/images/description.jpeg"
+        alt="products marketplace"
+        width={500}
+        height={300}
+      />
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
         <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
