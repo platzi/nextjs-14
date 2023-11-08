5214301ac139f6c502be9936c01dc6bc7cc8c59f
edevars <e.devars.dev@gmail.com>
2023-11-08 09:44:38 -0600

    feat: adding statics

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,13 @@
+import styles from './Description.module.sass';
+
 export const Description = () => {
-  return(
-    <section>
-      <h1>Descripción</h1>
+  return (
+    <section className={styles.Description}>
+      <img src="/images/description.jpeg" alt="products marketplace" />
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+      </div>
     </section>
   )
 }
