3dc80c6cc57b24d4c15a65b885a75643a77297b4
edevars <e.devars.dev@gmail.com>
2023-11-09 10:04:19 -0600

    feat: shopify api

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,17 @@
-"use client";
+const getProducts = async () => {
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+    headers: new Headers({
+      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+    })
+  })
+  const data = await response.json()
+  return data
+}
 
-export const MainProducts = () => {
-  console.log("variable de entorno",process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
-  return(
+export const MainProducts = async () => {
+  const products = await getProducts()
+  console.log(products)
+  return (
     <section>
       <h1>Main Products</h1>
     </section>
