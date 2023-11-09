b70eafccb94bfbff4b9f04a00d8c30ca849bd0b9
edevars <e.devars.dev@gmail.com>
2023-11-09 09:39:14 -0600

    feat: env-vars

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 export const MainProducts = () => {
+  console.log("variable de entorno",process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
   return(
     <section>
       <h1>Main Products</h1>
