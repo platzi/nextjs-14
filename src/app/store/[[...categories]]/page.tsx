f431f7c7e00687424c53423293919ae0f3344981
edevars <e.devars.dev@gmail.com>
2023-11-10 15:50:44 -0600

    feat: store end

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,3 +1,6 @@
+import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
+import { getProducts } from "app/services/shopify"
+
 interface CategoryProps {
   params: {
     categories: string[],
@@ -5,11 +8,11 @@ interface CategoryProps {
   searchParams?: string
 }
 
-export default function Category(props: CategoryProps){
-
+export default async function Category(props: CategoryProps){
+  const products = await getProducts()
   const { categories } = props.params
   // throw new Error('Error: boom')
   return(
-    <h1>Categoria dinámica: {categories}</h1>
+    <ProductsWrapper products={products}/>
   )
 }
