9b9f284922d0e1d6ca18a378b679ee026c51dc7b
edevars <e.devars.dev@gmail.com>
2023-11-10 16:29:04 -0600

    feat: styles

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,19 +1,23 @@
 import { getCollections } from "app/services/shopify/collections"
 import Link from "next/link"
+import styles from './StoreLayout.module.sass'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const collections = await getCollections()
 
   return (
-    <main>
+    <main className={styles.StoreLayout}>
+      <h1>Explore</h1>
       <nav>
-        {
-          collections.map((collection) => (
-            <Link key={collection.id} href={'/store/' + collection.handle}>
-              {collection.title}
-            </Link>
-          ))
-        }
+        <ul className={styles.StoreLayout__list}>
+          {
+            collections.map((collection) => (
+              <Link key={collection.id} href={'/store/' + collection.handle} className={styles.StoreLayout__chip}>
+                {collection.title}
+              </Link>
+            ))
+          }
+        </ul>
       </nav>
       {children}
     </main>
