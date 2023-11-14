121074fa626474bba11a6425c5d4269f41392bfb
edevars <e.devars.dev@gmail.com>
2023-11-14 11:01:41 -0600

    feat: product-view

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,12 +1,16 @@
-"use client"
-import { useParams, useSearchParams } from "next/navigation"
+import { ProductView } from "app/components/product/ProductView"
+import { getProducts } from "app/services/shopify/products"
 
+interface ProductPageProps {
+  searchParams: {
+    id: string
+  }
+}
 
-export default function ProductPage(){
-  const params = useParams()
-  const searchParams = useSearchParams()
-  const id = searchParams.get("id")
+export default async function ProductPage({ searchParams }: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
 
-  console.log('searchParams', id)
-  return <h1>Product Page</h1>
+  return <ProductView product={product} />
 }
--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -1,15 +1,30 @@
 import { env } from "app/config/env"
 import { shopifyUrls } from "./urls"
 
-export const getProducts = async () => {
+export const getProducts = async (id?: string): Promise<ProductType[]> => {
   try {
-    const response = await fetch(shopifyUrls.products.all, {
+    const apiUrl = id ? `${shopifyUrls.products.all}?ids=${id}` : shopifyUrls.products.all
+    const response = await fetch(apiUrl, {
       headers: new Headers({
         'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
       })
     })
     const { products } = await response.json()
-    return products
+
+    const transformedProducts = products.map((product: any) => {
+      return {
+        id: product.id,
+        gql_id: product.variants[0].admin_graphql_api_id,
+        title: product.title,
+        description: product.body_html,
+        price: product.variants[0].price,
+        image: product.images[0].src,
+        quantity: product.variants[0].inventory_quantity,
+        handle: product.handle,
+        tags: product.tags,
+      }
+    })
+    return transformedProducts
   } catch (error) {
     console.log(error)
   }
