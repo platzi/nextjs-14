10de752e6eb7d727777ec6bd0d871263df2add3e
edevars <e.devars.dev@gmail.com>
2023-11-14 11:07:23 -0600

    feat: adding main products services

--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -29,3 +29,15 @@ export const getProducts = async (id?: string): Promise<ProductType[]> => {
     console.log(error)
   }
 }
+
+export const getMainProducts = async () => {
+  const response = await fetch(shopifyUrls.products.mainProducts, {
+    headers: new Headers({
+      'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
+    })
+  })
+
+  const {products} = await response.json()
+
+  return products
+}
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -2,7 +2,8 @@ import { env } from "app/config/env"
 
 export const shopifyUrls = {
   products: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+    mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/273441587288/products.json`,
   },
   collections: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
