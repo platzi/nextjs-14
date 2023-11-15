b8faa024cf4c8e266596cb1e5432d98ba68b871b
edevars <e.devars.dev@gmail.com>
2023-11-15 09:39:15 -0600

    feat: adding login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import { LoginForm } from "app/components/login/LoginForm";
+
+export default function LoginPage(){
+  return <LoginForm />
+}
---
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -0,0 +1,15 @@
+"use client"
+import styles from "./LoginForm.module.sass";
+
+export const LoginForm = () => {
+  return (
+    <div className={styles.NewAccountForm}  >
+      <h1 className={styles.NewAccountForm__title}>New Account</h1>
+      <form className={styles.NewAccountForm__form}>
+        <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
+        <input type="password" name="password" placeholder="password" />
+        <input type="submit" name="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
