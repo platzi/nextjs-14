0658dd9255a654828adf399fe48bbc2336e78e48
edevars <e.devars.dev@gmail.com>
2023-11-15 09:34:03 -0600

    feat: adding access-cookie

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,8 @@
 "use server"
 import { GraphQLClientSingleton } from "app/graphql"
 import { createUserMutation } from "app/graphql/mutations/createUserMutation"
+import { createAccessToken } from "app/utils/auth/createAccessToken"
+import { redirect } from "next/navigation"
 
 export const handleCreateUser = async (formData: FormData) => {
   const formDataObject = Object.fromEntries(formData)
@@ -15,6 +17,8 @@ export const handleCreateUser = async (formData: FormData) => {
 
   const { customerCreate } = await graphqlClient.request(createUserMutation, variables)
   const { customerUserErrors, customer } = customerCreate
-  console.log(customer)
-  console.log(customerUserErrors)
+  if(customer?.firstName){
+    await createAccessToken(formDataObject.email as string, formDataObject.password as string)
+    redirect('/store')
+  }
 }
---
+++ src/utils/auth/createAccessToken.ts
@@ -0,0 +1,23 @@
+import { GraphQLClientSingleton } from "app/graphql"
+import { customerAccessTokenCreateMutation } from "app/graphql/mutations/customerAccessTokenCreate"
+import { cookies } from 'next/headers'
+
+export const createAccessToken = async (email: string, password: string) => {
+  const cookiesStore = cookies()
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const { customerAccessTokenCreate } = await graphqlClient.request(customerAccessTokenCreateMutation, {
+    "email": email,
+    "password": password
+  })
+
+  const { accessToken, expiresAt } = customerAccessTokenCreate?.customerAccessToken
+
+  if(accessToken){
+    cookiesStore.set("accessToken", accessToken, {
+      path: "/",
+      expires: new Date(expiresAt),
+      httpOnly: true,
+      sameSite: "strict"
+    })
+  }
+}
