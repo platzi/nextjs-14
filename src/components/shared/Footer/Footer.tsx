ac9fff104225086a49b730687dbef88c8096f7bf
edevars <e.devars.dev@gmail.com>
2023-11-15 10:50:36 -0600

    feat: new styles

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import styles from './Footer.module.sass';
+
 export const Footer = () => {
   return(
-    <footer>
-      <p>Future World</p>
+    <footer className={styles.Footer}>
+      <p>Future World © {new Date().getFullYear()}</p>
     </footer>
   )
-}
+};
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
-import styles from './Header.module.css'
 import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
+import styles from './Header.module.sass'
 
 
 export const Header = async () => {
   const customer = await validateAccessToken()
 
   return (
-    <header>
+    <header className={styles.Header}>
       <nav>
         <ul className={styles.Header__list}>
           <li>
@@ -21,7 +21,9 @@ export const Header = async () => {
             </Link>
           </li>
         </ul>
-        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
       </nav>
+      <div className={styles.Header__user}>
+        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
+      </div>
     </header>)
 }
