d9269cbe828827939d1b27061802fd3c8dae55f3
edevars <e.devars.dev@gmail.com>
2023-11-15 08:29:20 -0600

    feat: server actions end

---
+++ src/actions/index.ts
@@ -0,0 +1,5 @@
+"use server"
+
+export const handleCreateUser = (formData) => {
+  console.log("handleCreateUser", formData)
+}
--- src/components/signup/NewAccountForm/NewAccountForm.tsx
+++ src/components/signup/NewAccountForm/NewAccountForm.tsx
@@ -1,17 +1,23 @@
 "use client";
 import { useState } from "react";
 import styles from "./NewAccountForm.module.sass";
+import { handleCreateUser } from "app/actions";
 
 export const NewAccountForm = () => {
 
   const [errors, setErrors] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    handleCreateUser(formData)
+  }
 
   return (
     <div className={styles.NewAccountForm}  >
       <h1 className={styles.NewAccountForm__title}>New Account</h1>
-      <form className={styles.NewAccountForm__form}>
+      <form className={styles.NewAccountForm__form} onSubmit={handleSubmit}>
         <input type="text" name="first_name" placeholder="Name" disabled={loading} />
         <input type="text" name="last_name" placeholder="Lastname" disabled={loading} />
         <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
