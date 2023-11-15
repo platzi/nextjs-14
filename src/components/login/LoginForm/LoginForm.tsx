7c6ca74d6c6b4bb5fc933c087806e5035d97a0aa
edevars <e.devars.dev@gmail.com>
2023-11-15 09:40:58 -0600

    fix: changing title

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -4,7 +4,7 @@ import styles from "./LoginForm.module.sass";
 export const LoginForm = () => {
   return (
     <div className={styles.NewAccountForm}  >
-      <h1 className={styles.NewAccountForm__title}>New Account</h1>
+      <h1 className={styles.NewAccountForm__title}>Login</h1>
       <form className={styles.NewAccountForm__form}>
         <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
         <input type="password" name="password" placeholder="password" />
