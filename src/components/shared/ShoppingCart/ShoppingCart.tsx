c8d1cafdb121c16d6e39afd19446b193c6e56069
edevars <e.devars.dev@gmail.com>
2023-11-15 12:38:41 -0600

    feat: adding cart items display

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,17 +1,35 @@
 "use client";
+import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import styles from './ShoppingCart.module.sass'
 import { useShoppingCart } from "app/hooks/useShoppingCart";
+import styles from './ShoppingCart.module.sass'
 
 export const ShoppingCart = () => {
   const { cart } = useShoppingCart();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <button className={styles.ShoppingCart}>
+    <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>
         {cart.length}
       </span>
       <FaShoppingCart />
+      {isOpen && (
+        <div className={styles.ShoppingCart__items}>
+          {
+            cart.map(item => (
+              <>
+                <p key={item?.id}>{item?.title}</p>
+                <p>Cantidad: {item.quantity}</p>
+              </>
+            ))
+          }
+          <button className={styles.ShoppingCart__buyButton}>
+            Buy
+          </button>
+        </div>
+      )}
     </button>
   )
 }
