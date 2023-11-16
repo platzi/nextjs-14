41e1322297425189bd490aaed7fe422a09084ad4
edevars <e.devars.dev@gmail.com>
2023-11-16 10:46:02 -0600

    feat: adding agent

---
+++ src/utils/openai/createAgent.tsx
@@ -0,0 +1,11 @@
+const createAgent = (productTitles: string) => {
+  return `
+  Eres un vendedor de una tienda en linea que tiene los siguientes productos. 
+
+  ${productTitles}
+
+  Recomienda productos de los anteriormente listados.
+
+  La respuesta tiene que ser convincente y mostrar todas las ventajas de este producto. Usa respuestas cortas y carismáticas.
+  `
+}
