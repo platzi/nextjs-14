ddd5725e4a95814b5ab884127091622fd14e20f2
edevars <e.devars.dev@gmail.com>
2023-11-16 10:30:59 -0600

    feat: adding chat bot

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import { Chat } from "app/components/chat/chat"
+
+export default function ChatPage() {
+  return (
+    <>
+      <h1>Chatbot</h1>
+      <Chat />
+    </>
+  )
+}
---
+++ src/components/chat/chat.tsx
@@ -0,0 +1,34 @@
+'use client';
+ 
+import { useChat } from 'ai/react';
+ 
+export const Chat = () => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+ 
+  return (
+    <main className="mx-auto w-full h-screen max-w-lg p-24 flex flex-col">
+      <section className="mb-auto m">
+        {messages.map(m => (
+          <div className="mb-4" key={m.id}>
+            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.content}
+          </div>
+        ))}
+      </section>
+      <form className="flex space-x-4" onSubmit={handleSubmit}>
+        <input
+          className="rounded-md p-2 text-black"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Say something..."
+        />
+        <button
+          className="border-solid border-2 border-white p-2 rounded-md"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </main>
+  );
+}
