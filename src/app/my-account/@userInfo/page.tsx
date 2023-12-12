82d37b5835f8a8e740245abaaf292228a8895f1b
edevars <e.devars.dev@gmail.com>
2023-12-12 10:32:52 -0600

    feat: adding dynamic render

--- src/app/my-account/@userInfo/page.tsx
+++ src/app/my-account/@userInfo/page.tsx
@@ -1,5 +1,7 @@
 import { validateAccessToken } from "app/utils/auth/validateAccessToken";
 
+export const dynamic = 'force-dynamic';
+
 export default async function MyAccountPage() {
   const customer = await validateAccessToken();
 
